e4652adf1d8c36dc87ecf28eae052e871a076cc7
Jipu Wang <jipuwang@umich.edu>
2017-05-23 19:18:34 -0400

    added continuous MMS and MMS source, this version now gives you the right order of accuracy

--- Sn_1D_kord_MMSSrc_cont_flux.m
+++ Sn_1D_kord_MMSSrc_cont_flux.m
@@ -9,7 +9,7 @@
 % Time: steady state.
 % MMS Source: Fixed specified source, angle-dependent
 %% Main Function
-function [ ] = Sn_1D_kord_MMSSrc()
+function [ ] = Sn_1D_kord_MMSSrc_cont_flux()
 %     FDMs=[1,2,3];
     FDMs=[2];
     % 1:Step; 2:DD; 3:SC;
@@ -24,7 +24,7 @@ function [ ] = Sn_1D_spatial(FDM)
 %     nCells=160*[1,2,4,8,16];
 %     nCells=[10,20,40,80];
 %     nCells=[60];
-    nCells=[1,2,4]*60*2%*4*4*4;
+    nCells=[1,2,4,8]*60*2%*4*4*4;
     nRuns=size(nCells,2);
     RMSs_phi0_g1=zeros(nRuns,1);
     RMSs_phi0_g2=zeros(nRuns,1);
@@ -51,8 +51,8 @@ function [ ] = Sn_1D_spatial(FDM)
     p_phi0_g1=zeros(nRuns-1,1);
     p_phi0_g2=zeros(nRuns-1,1);
     for i=1:nRuns-1
-        p_phi0_g1=log(RMSs_phi0_g1(i)/RMSs_phi0_g1(i+1))/log(nCells(i+1)/nCells(i));
-        p_phi0_g2=log(RMSs_phi0_g2(i)/RMSs_phi0_g2(i+1))/log(nCells(i+1)/nCells(i));
+        p_phi0_g1(i)=log(RMSs_phi0_g1(i)/RMSs_phi0_g1(i+1))/log(nCells(i+1)/nCells(i));
+        p_phi0_g2(i)=log(RMSs_phi0_g2(i)/RMSs_phi0_g2(i+1))/log(nCells(i+1)/nCells(i));
     end
     
     display(p_phi0_g1);
@@ -151,73 +151,88 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
             alpha_n=coth(0.5*tao_n)-2./tao_n;
     end % switch FDM
 
-%% For the fast group
+%% For fast group
+    % Region 1: 
+    reg1x2=+0.0020912236785901;
+    reg1x1=-0.0088038653251593;
+    reg1x0=+0.0345867334422320;
+    % Region 2:
+    reg2x2=-0.0044008146496444;
+    reg2x1=+0.2639624489114600;
+    reg2x0=-2.8239242199963540;
+    % Region 3:
+    reg3x2=+0.0019059413144357;
+    reg3x1=-0.2235676470181600;
+    reg3x0=+6.5864700746602860;
+
+    reg1 =@(x) +reg1x2*x.^2 ...
+               +reg1x1*x ...
+               +reg1x0;
+    reg2 =@(x) +reg2x2*x.^2 ...
+               +reg2x1*x ...
+               +reg2x0;
+    reg3 =@(x) +reg3x2*x.^2 ...
+               +reg3x1*x ...
+               +reg3x0;
     % Bounday conditions and source
-    % phi=Reg1:y = +1.5593897658139900E-03 x2 -1.3533502228265900E-03 x +6.3255899575709700E-02
-    psi_b1_n=0.5*(6.3255899575709700E-02)*ones(N,1); % the -tive half N/2 are not useful
-    % phi=Reg3:y = +1.6006807673739400E-03 x2 -1.8963240555487500E-01 x +5.6794357067491300E+00
-    psi_b2_n=0.5*(+1.6006807673739400E-03*Z*Z ...
-        -1.8963240555487500E-01*Z ...
-        +5.6794357067491300E+00)*ones(N,1); % the +tive half N/2 are not useful
+    % reg1 expression evaluated at x=0 and take half
+    psi_b1_n=0.5*reg1(0)*ones(N,1); % the -tive half N/2 are not useful
+    % reg3 expression evaluated at x=Z and take half
+    psi_b2_n=0.5*reg3(Z)*ones(N,1); % the +tive half N/2 are not useful
 
     Q_MMS_n_j_g1=zeros(N,J); % preallocate memory, avg'ed over tau_(j-1/2) and tau_(j+1/2)
+    % MMS source: mu_n/2 * derivative(phi_MMS_g1) ...
+    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_g1
     phi0_MMS_j_g1=zeros(J,1);
                                 
     % Fast MMS Source in Region 1
-    % phi_MMS_1g = ( +1.5593897658139900E-03 x^2 -1.3533502228265900E-03 x +6.3255899575709700E-02 )
-    % mu_n/2 * ( +3.1187795316279800E-03 x -1.3533502228265900E-03 ) ...
-    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_1g
-    % (Sig_t-Sig_ss)*0.5 in this case is 1.5034250000000000E-02
+    % phi_MMS_g1 = reg1
     for j=1:J/3
         phi0_MMS_j_g1(j)= ...
-                ( +1.5593897658139900E-03 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  -1.3533502228265900E-03 *0.5*(j-1+j)*h ...
-                  +6.3255899575709700E-02 );
+                ( +reg1x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg1x1 *0.5*(j-1+j)*h ...
+                  +reg1x0 );
         for n=1:N
-        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(+3.1187795316279800E-03 *0.5*(j-1+j)*h -1.3533502228265900E-03) ...
+        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(2*reg1x2 *0.5*(j-1+j)*h +reg1x1) ...
             +(Sig_t_g1(j)-Sig_ss_g1(j))*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
     % Fast MMS Source in Region 2
-    % phi_MMS_1g = ( -4.4443431764232400E-03 x^2 +2.6590845920514000E-01 x -2.8376294939004300E+00 )
-    % mu_n/2 * ( -8.8886863528464800E-03 x +2.6590845920514000E-01 ) ...
-    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_1g
-    % (Sig_t-Sig_ss)*0.5 in this case is 1.6119000000000000E-02
+    % For the next two regions, nothing changed exception for the coeff. 
+    % phi_MMS_g1 = reg2
     for j=J/3+1:2/3*J
         phi0_MMS_j_g1(j)= ...
-                ( -4.4443431764232400E-03 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  +2.6590845920514000E-01 *0.5*(j-1+j)*h ...
-                  -2.8376294939004300E+00 );
+                ( +reg2x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg2x1 *0.5*(j-1+j)*h ...
+                  +reg2x0 );
         for n=1:N
-        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(-8.8886863528464800E-03 *0.5*(j-1+j)*h +2.6590845920514000E-01) ...
+        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(2*reg2x2 *0.5*(j-1+j)*h +reg2x1) ...
             +(Sig_t_g1(j)-Sig_ss_g1(j))*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
     % Fast MMS Source in Region 3
-    % phi_MMS_1g = ( +1.6006807673739400E-03 x^2 -1.8963240555487500E-01 x +5.6794357067491300E+00 )
-    % mu_n/2 * ( +3.2013615347478800E-03 x -1.8963240555487500E-01 ) ...
-    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_1g
-    % (Sig_t-Sig_ss)*0.5 in this case is 1.5034250000000000E-02
+    % phi_MMS_g1 = reg3
     for j=2/3*J+1:J
-      phi0_MMS_j_g1(j)= ...
-                ( +1.6006807673739400E-03 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  -1.8963240555487500E-01 *0.5*(j-1+j)*h ...
-                  +5.6794357067491300E+00 );
+        phi0_MMS_j_g1(j)= ...
+                ( +reg3x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg3x1 *0.5*(j-1+j)*h ...
+                  +reg3x0 );
         for n=1:N
-        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(+3.2013615347478800E-03 *0.5*(j-1+j)*h -1.8963240555487500E-01) ...
+        Q_MMS_n_j_g1(n,j)=mu_n(n)*0.5*(2*reg3x2 *0.5*(j-1+j)*h +reg3x1) ...
             +(Sig_t_g1(j)-Sig_ss_g1(j))*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
 
-%% sacttering source iteration
-    S_n_j=zeros(N,J);
+%% Non-MMS stuff
+    % Start converging self-scattering
+    self_scat_n_j=zeros(N,J);
     kMax=2000;
     for k=1:kMax
         if k==1
             phi0_g1_j_old=ones(J,1);
         end
         for j=1:J
-            S_n_j(:,j)=0.5*(Sig_ss_g1(j)*phi0_g1_j_old(j));
+            self_scat_n_j(:,j)=0.5*(Sig_ss_g1(j)*phi0_g1_j_old(j));
         end % j
 
         %determine the flux based on this source.
@@ -229,7 +244,7 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
                     psi_n_in=psi_b1_n(n);
                 end
                 temp=mu_n(n)+alpha_n(n)*Sig_t_g1(j)*h*0.5;
-                psi_n_out=((temp-0.5*Sig_t_g1(j)*h)*psi_n_in+(S_n_j(n,j)+Q_MMS_n_j_g1(n,j))*h)/(temp+0.5*Sig_t_g1(j)*h);
+                psi_n_out=((temp-0.5*Sig_t_g1(j)*h)*psi_n_in+(self_scat_n_j(n,j)+Q_MMS_n_j_g1(n,j))*h)/(temp+0.5*Sig_t_g1(j)*h);
                 psi_n_avg=0.5*(1+alpha_n(n))*psi_n_out+0.5*(1-alpha_n(n))*psi_n_in;
                 phi0_j_g1(j)=phi0_j_g1(j)+psi_n_avg*weight_n(n);
                 psi_n_in=psi_n_out;
@@ -242,7 +257,7 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
                     psi_n_in=psi_b2_n(n);
                 end
                 temp=abs(mu_n(n))+abs(alpha_n(n))*Sig_t_g1(j)*h*0.5;
-                psi_n_out=((temp-0.5*Sig_t_g1(j)*h)*psi_n_in+(S_n_j(n,j)+Q_MMS_n_j_g1(n,j))*h)/(temp+0.5*Sig_t_g1(j)*h);
+                psi_n_out=((temp-0.5*Sig_t_g1(j)*h)*psi_n_in+(self_scat_n_j(n,j)+Q_MMS_n_j_g1(n,j))*h)/(temp+0.5*Sig_t_g1(j)*h);
                 psi_n_avg=0.5*(1+abs(alpha_n(n)))*psi_n_out+0.5*(1-abs(alpha_n(n)))*psi_n_in;
                 phi0_j_g1(j)=phi0_j_g1(j)+psi_n_avg*weight_n(n);
                 psi_n_in=psi_n_out;
@@ -264,79 +279,98 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
     title('fast flux');
 
 %% For thermal group
+    % Region 1: 
+    reg1x2=+0.00038256921789935;
+    reg1x1=+0.00372575820304040;
+    reg1x0=+0.00245833122976550;
+    % Region 2:
+    reg2x2=-0.00116713935976200;
+    reg2x1=+0.07017666404691600;
+    reg2x0=-0.70667635458320700;
+    % Region 3:
+    reg3x2=+0.00038371079460677;
+    reg3x1=-0.04964502056042800;
+    reg3x0=+1.60483078272052200;
+    
+    reg1 =@(x) +reg1x2*x.^2 ...
+               +reg1x1*x ...
+               +reg1x0;
+    reg2 =@(x) +reg2x2*x.^2 ...
+               +reg2x1*x ...
+               +reg2x0;
+    reg3 =@(x) +reg3x2*x.^2 ...
+               +reg3x1*x ...
+               +reg3x0;
     % Bounday conditions and source
-    % phi=Reg1:y = +3.7420664087460500E-04x2 +3.2444814149334100E-03x +1.2139249518777500E-02
-    psi_b1_n=0.5*1.2139249518777500E-02*ones(N,1); % the -tive half N/2 are not useful
-    % phi=Reg3:y = +3.6321032591887700E-04x2 -4.7314818437341200E-02x +1.5394028032516800E+00
-    psi_b2_n=0.5*(+3.6321032591887700E-04*Z*Z ...
-                  -4.7314818437341200E-02*Z ...
-                  +1.5394028032516800E+00)*ones(N,1); % the +tive half N/2 are not useful
-    Q_n_j_g2=zeros(N,J); % Inscatter/downscatter from group 1. 
-    for j=1:J
-        Q_n_j_g2(:,j)=0.5*Sig_rm_g1(j)*phi0_j_g1(j);
-    end % j
+    % reg1 expression evaluated at x=0 and take half
+    psi_b1_n=0.5*reg1(0)*ones(N,1); % the -tive half N/2 are not useful
+    % reg3 expression evaluated at x=Z and take half
+    psi_b2_n=0.5*reg3(Z)*ones(N,1); % the +tive half N/2 are not useful
+
 
     Q_MMS_n_j_g2=zeros(N,J);
+    % MMS source: mu_n/2 * derivative(phi_MMS_g2) ...
+    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_g2
+    % - Sig_g1_rm*0.5 * (phi_MMS_g1)
     phi0_MMS_j_g2=zeros(J,1);
     % Thermal MMS Source in Region 1
-    % phi_MMS_2g = ( +3.7420664087460500E-04 x^2 +3.2444814149334100E-03 x +1.2139249518777500E-02 )
-    % mu_n/2 * ( +7.4841328174921000E-04 x +3.2444814149334100E-03 ) ...
-    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_g2
-    % - Sig_g1_rm*0.5 * (phi_g1_MMS)
-    % (Sig_t-Sig_ss)*0.5* in this case is +4.4870000000000200E-03
+    % phi_MMS_g1 = reg1
     for j=1:J/3
         phi0_MMS_j_g2(j)= ...
-                ( +3.7420664087460500E-04 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  +3.2444814149334100E-03 *0.5*(j-1+j)*h ...
-                  +1.2139249518777500E-02 );
+                ( +reg1x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg1x1 *0.5*(j-1+j)*h ...
+                  +reg1x0 );
         for n=1:N
-        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(+7.4841328174921000E-04 *0.5*(j-1+j)*h +3.2444814149334100E-03) ...
+        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(2*reg1x2 *0.5*(j-1+j)*h +reg1x1) ...
             +(Sig_t_g2(j)-Sig_ss_g2(j))*0.5 * phi0_MMS_j_g2(j) ...
             -Sig_rm_g1(j)*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
     % Thermal MMS Source in Region 2
-    % phi_MMS_g2 = ( -1.2351628643713800E-03 x^2 +7.3932137536755800E-02 x -7.5807942857102300E-01 )
-    % mu_n/2 * ( -2.4703257287427600E-03 x +7.3932137536755800E-02 ) ...
-    % + (Sig_t-Sig_ss)*0.5 * phi_MMS_g2
-    % - Sig_g1_rm*0.5 * (phi_MMS_g1)
-    % (Sig_t-Sig_ss)*0.5 in this case is +1.1775000000000000E-01
+    % phi_MMS_g1 = reg2
+    % The only difference in the following 2 regions are
+    % reg2x2,reg2x1,reg2x0
     for j=J/3+1:2/3*J
         phi0_MMS_j_g2(j)= ...
-                ( -1.2351628643713800E-03 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  +7.3932137536755800E-02 *0.5*(j-1+j)*h ...
-                  -7.5807942857102300E-01 );
+                ( +reg2x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg2x1 *0.5*(j-1+j)*h ...
+                  +reg2x0 );
         for n=1:N
-        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(-2.4703257287427600E-03 *0.5*(j-1+j)*h +7.3932137536755800E-02) ...
+        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(2*reg2x2 *0.5*(j-1+j)*h +reg2x1) ...
             +(Sig_t_g2(j)-Sig_ss_g2(j))*0.5 * phi0_MMS_j_g2(j) ...
             -Sig_rm_g1(j)*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
 
     % Thermal MMS Source in Region 3
-    % phi_MMS_g2 = ( +3.6321032591887700E-04 x^2 -4.7314818437341200E-02 x +1.5394028032516800E+00 )
-    % mu_n/2* ( +7.2642065183775400E-04 x -4.7314818437341200E-02 ) ...
-    % + (Sig_t-Sig_ss)*0.5* phi_MMS_g2
-    % - Sig_g1_rm*0.5 * (phi_MMS_g1)
-    % (Sig_t-Sig_ss)*0.5 in this case is +4.4870000000000200E-03
+    % phi_MMS_g1 = reg3
     for j=2/3*J+1:J
         phi0_MMS_j_g2(j)= ...
-                ( +3.6321032591887700E-04 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
-                  -4.7314818437341200E-02 *0.5*(j-1+j)*h ...
-                  +1.5394028032516800E+00 );
+                ( +reg3x2 /3*((j-1)*(j-1)+(j-1)*j+j*j)*h*h ...
+                  +reg3x1 *0.5*(j-1+j)*h ...
+                  +reg3x0 );
         for n=1:N
-        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(+7.2642065183775400E-04 *0.5*(j-1+j)*h -4.7314818437341200E-02) ...
+        Q_MMS_n_j_g2(n,j)=mu_n(n)*0.5*(2*reg3x2 *0.5*(j-1+j)*h +reg3x1) ...
             +(Sig_t_g2(j)-Sig_ss_g2(j))*0.5 * phi0_MMS_j_g2(j) ...
             -Sig_rm_g1(j)*0.5 * phi0_MMS_j_g1(j);
         end % n
     end % j
 
+%% Non-MMS stuff
+    % Inscattering source component
+    inscat_n_j_g2=zeros(N,J); % Inscatter/downscatter from group 1. 
+    for j=1:J
+        inscat_n_j_g2(:,j)=0.5*Sig_rm_g1(j)*phi0_j_g1(j);
+    end % j
+    
+    % Start converging self-scattering
+    self_scat_n_j=zeros(N,J);
     for k=1:kMax
         if k==1
             phi0_j_g2_old=ones(J,1);
         end
         for j=1:J
-            S_n_j(:,j)=0.5*(Sig_ss_g2(j)*phi0_j_g2_old(j));
+            self_scat_n_j(:,j)=0.5*(Sig_ss_g2(j)*phi0_j_g2_old(j));
         end % j
         %determine the flux based on this source.
         phi0_j_g2=zeros(J,1);
@@ -347,7 +381,7 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
                     psi_n_in=psi_b1_n(n);
                 end
                 temp=mu_n(n)+alpha_n(n)*Sig_t_g2(j)*h*0.5;
-                psi_n_out=((temp-0.5*Sig_t_g2(j)*h)*psi_n_in+(S_n_j(n,j)+Q_n_j_g2(n,j)+Q_MMS_n_j_g2(n,j))*h)/(temp+0.5*Sig_t_g2(j)*h);
+                psi_n_out=((temp-0.5*Sig_t_g2(j)*h)*psi_n_in+(self_scat_n_j(n,j)+inscat_n_j_g2(n,j)+Q_MMS_n_j_g2(n,j))*h)/(temp+0.5*Sig_t_g2(j)*h);
                 psi_n_avg=0.5*(1+alpha_n(n))*psi_n_out+0.5*(1-alpha_n(n))*psi_n_in;
                 phi0_j_g2(j)=phi0_j_g2(j)+psi_n_avg*weight_n(n);
                 psi_n_in=psi_n_out;
@@ -360,7 +394,7 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
                     psi_n_in=psi_b2_n(n);
                 end
                 temp=abs(mu_n(n))+abs(alpha_n(n))*Sig_t_g2(j)*h*0.5;
-                psi_n_out=((temp-0.5*Sig_t_g2(j)*h)*psi_n_in+(S_n_j(n,j)+Q_n_j_g2(n,j)+Q_MMS_n_j_g2(n,j))*h)/(temp+0.5*Sig_t_g2(j)*h);
+                psi_n_out=((temp-0.5*Sig_t_g2(j)*h)*psi_n_in+(self_scat_n_j(n,j)+inscat_n_j_g2(n,j)+Q_MMS_n_j_g2(n,j))*h)/(temp+0.5*Sig_t_g2(j)*h);
                 psi_n_avg=0.5*(1+abs(alpha_n(n)))*psi_n_out+0.5*(1-abs(alpha_n(n)))*psi_n_in;
                 phi0_j_g2(j)=phi0_j_g2(j)+psi_n_avg*weight_n(n);
                 psi_n_in=psi_n_out;
@@ -381,7 +415,7 @@ function [error_j_g1,error_j_g2]=Sn_1D_RMS(J,FDM)
     % plots
     figure(14);plot(phi0_j_g2,'-*'); title('thermal flux');
     figure(15);plot(phi0_j_g1,'-*'); hold on; plot(phi0_j_g2,'-*');
-    title('with Manufactured Source');
+    title('with Continuous Manufactured Source');
     legend('fast flux','thermal flux'); hold off;
     
     % getter for regression test
