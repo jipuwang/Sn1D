c1e21ded7a72313244732c322416c6527903756e
Jipu Wang <jipuwang@umich.edu>
2017-08-18 17:09:47 -0400

    an overhaul change so that it deals with an eigenvalue problem

--- 2016PhysorCode/converger_SnEig.m
+++ 2016PhysorCode/converger_SnEig.m
@@ -9,7 +9,7 @@
 % the problem description. 
 % It needs to know the geometry and is responsible for generating the grid
 % and pass the grid information to the coupler. 
-function [order_phi]=converger_SnEig(assumedSoln)
+function [order_phi0]=converger_SnEig(assumedSoln,assumedK)
 % clear;
 nGrids=4%8%4%4%6;%10;%8;
 refinementRatio=2;
@@ -27,12 +27,17 @@ if ~exist('assumedSoln','var')
 %   assumedSoln='flat_expMu';
 end
 
-error_phi0_n=zeros(nGrids,1);
-gridMeshSize_n=zeros(nGrids,1);
+if ~exist('k_MMS','var')
+  assumedK=1.02;
+end
+
+gridMeshSize_iGrid=zeros(nGrids,1);
+error_phi0_iGrid=zeros(nGrids,1);
+error_k_iGrid=zeros(nGrids,1);
 
 for iGrid=1:nGrids
   J=5*refinementRatio^iGrid;
-  gridMeshSize_n(iGrid)=Tau/J;
+  gridMeshSize_iGrid(iGrid)=Tau/J;
   iGrid
   % Material
   field1='Sig_t_j';          value1=ones(J,1);
@@ -46,58 +51,41 @@ for iGrid=1:nGrids
     field4,value4,field5,value5,field6,value6,field7,value7);
 
   [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j]=... 
-        manufacturer_Sn(J,N,Tau,mat,assumedSoln);
+        manufacturer_SnEig(J,N,Tau,mat,assumedSoln,assumedK);
 %   error_ang_j=error_ang_j*0.0;
-  [phi0_j]=Sn_module(J,N,Tau,mat,...
+  [k,phi0_j]=SnEig_module(J,N,Tau,mat,...
     psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j);
 
   % Calculate the error compared to manufactured solution
 %   error_ang_j=zeros(J,1);
 % error_ang_j=error_ang_j.*0.0;
-  error_phi0_n(iGrid)=norm(phi0_j-phi0_j_ana-error_ang_j,2)/sqrt(J) 
-  
-%   % Plot the solution
-%   figure(11);
-%   plot(phi0_j,'*-');
-%   hold on;
-%   grid on;
-%   switch assumedSoln
-%     case 'sine_sine_sine'
-%       phi0_MMS =@(x) (sin(pi/(size(phi0_j,1)).*x)+1)*4.090350086939905;
-%     case 'sine_exp_exp'
-%       phi0_MMS =@(x) (sin(pi/(size(phi0_j,1)).*x)+1)*37.102114262431876;
-%     case 'IHM'
-%       phi0_MMS =@(x) 2.0+0.0*x;
-%   end
-%   
-%   fplot(phi0_MMS,[0,size(phi0_j,1)],'bo-');
-%   legend('numerical','analytical');
-%   title('scalar flux');
-%   xlabel('mesh size [cm]');
-%   ylabel('scalar flux');
+  error_phi0_iGrid(iGrid)=norm(phi0_j-phi0_j_ana-error_ang_j,2)/sqrt(J)
+  error_k_iGrid(iGrid)=k-assumedK
   
 end
-% figure(11); hold off;
 
 % Calculate the order of accuracy
-order_phi_nMinus1=ones(nGrids-1,1);
+order_phi0_nMinus1=ones(nGrids-1,1);
+order_k_nMinus1=ones(nGrids-1,1);
 for j=1:nGrids-1
-  order_phi_nMinus1(j)=log(error_phi0_n(j)/error_phi0_n(j+1)) / ...
-    log(gridMeshSize_n(j)/gridMeshSize_n(j+1));
+  order_phi0_nMinus1(j)=log(error_phi0_iGrid(j)/error_phi0_iGrid(j+1)) / ...
+    log(gridMeshSize_iGrid(j)/gridMeshSize_iGrid(j+1));
+  order_k_nMinus1(j)=log(error_k_iGrid(j)/error_k_iGrid(j+1)) / ...
+    log(gridMeshSize_iGrid(j)/gridMeshSize_iGrid(j+1));
 end
 
 %% Visualize the asymptotic convergence
-orderPlotGrid=[gridMeshSize_n(1) gridMeshSize_n(end)];
+orderPlotGrid=[gridMeshSize_iGrid(1) gridMeshSize_iGrid(end)];
 
 scalarFluxErrorRMS_plot_handle=figure(13);
-loglog(gridMeshSize_n,error_phi0_n,'*');
+loglog(gridMeshSize_iGrid,error_phi0_iGrid,'*');
 % title('scalar flux error convergence');
 xlabel('mesh size [cm]');
 ylabel('scalar flux error RMS');
 
 hold on;
-orderGuess=round(order_phi_nMinus1(end));
-errorStt=error_phi0_n(end)*refinementRatio^(orderGuess*(nGrids-1));
+orderGuess=round(order_phi0_nMinus1(end));
+errorStt=error_phi0_iGrid(end)*refinementRatio^(orderGuess*(nGrids-1));
 firstOrder=[errorStt errorStt/refinementRatio^(nGrids-1)];
 secondOrder=[errorStt errorStt/refinementRatio^(2*(nGrids-1))];
 thirdOrder=[errorStt errorStt/refinementRatio^(3*(nGrids-1))];
@@ -110,15 +98,40 @@ legend('scalar flux error','1st Order','2nd Order',...
   '3rd Order','4th Order','location','best');
 hold off;
 
+kErrorRMS_plot_handle=figure(14);
+loglog(gridMeshSize_iGrid,error_k_iGrid,'*');
+% title('scalar flux error convergence');
+xlabel('mesh size [cm]');
+ylabel('k error');
+
+hold on;
+orderGuess=round(order_k_nMinus1(end));
+errorStt=error_phi0_iGrid(end)*refinementRatio^(orderGuess*(nGrids-1));
+firstOrder=[errorStt errorStt/refinementRatio^(nGrids-1)];
+secondOrder=[errorStt errorStt/refinementRatio^(2*(nGrids-1))];
+thirdOrder=[errorStt errorStt/refinementRatio^(3*(nGrids-1))];
+fourthOrder=[errorStt errorStt/refinementRatio^(4*(nGrids-1))];
+loglog(orderPlotGrid,firstOrder,'--');
+loglog(orderPlotGrid,secondOrder,'--');
+loglog(orderPlotGrid,thirdOrder,'--');
+loglog(orderPlotGrid,fourthOrder,'--');
+legend('k error','1st Order','2nd Order',...
+  '3rd Order','4th Order','location','best');
+hold off;
+
 %% Dispaly the result
 % Display the problem description and results
 disp '=================';
 display(['assumedSoln: ' assumedSoln]);
 display(['refinementRatio: ' num2str(refinementRatio)]);
 display(['quad set order: ' num2str(N)]);
-error_phi0_n
-order_phi_nMinus1
-display(num2str(order_phi_nMinus1(end)));
-order_phi=order_phi_nMinus1(end);
+error_phi0_iGrid
+order_phi0_nMinus1
+display(num2str(order_phi0_nMinus1(end)));
+error_k_iGrid
+order_k_nMinus1
+
+order_phi0=order_phi0_nMinus1(end);
+order_k=order_k_nMinus1(end);
 
 end
