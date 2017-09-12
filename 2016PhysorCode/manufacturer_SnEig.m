82992872aa16f67d68a1486ac2fac7e58f3d6999
Jipu Wang <jipuwang@umich.edu>
2017-09-12 13:58:23 -0400

    added exponential angular dependence

--- 2016PhysorCode/manufacturer_SnEig.m
+++ 2016PhysorCode/manufacturer_SnEig.m
@@ -69,8 +69,8 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j,phi0_guess_j,k_gues
       psi_MMS =@(x,mu) (1.0+0.0*x).*exp(mu);
       psi_MMS_Diff =@(x,mu) (0.0+0.0*x).*exp(mu);
     case('sine')
-      psi_MMS =@(x,mu) sin(pi*x/Tau)+0.0*mu;%.*exp(mu);
-      psi_MMS_Diff =@(x,mu) pi/Tau*cos(pi*x/Tau)+0.0*mu;%.*exp(mu);
+      psi_MMS =@(x,mu) sin(pi*x/Tau).*exp(mu);
+      psi_MMS_Diff =@(x,mu) pi/Tau*cos(pi*x/Tau).*exp(mu);
   end
   
   Sig_gamma =@(x) Sig_gamma_j(1)+0.0*x;
