579bea2647ffda97d16dd215c842f3c969fcdaec
Jipu Wang <jipuwang@umich.edu>
2017-09-13 15:08:24 -0400

    changed '_' in case name to '-' to avoid subscript confusino

--- 2016PhysorCode/manufacturer_SnEig.m
+++ 2016PhysorCode/manufacturer_SnEig.m
@@ -65,13 +65,13 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,error_ang_j,phi0_guess_j,k_gues
       % Manufactured neutronics solution \psi(x,\mu)=1.0, 0<x<Tau
       psi_MMS =@(x,mu) sqrt(x+1).*(1.0+0.0*mu);
       psi_MMS_Diff =@(x,mu) 0.5./sqrt(x+1).*(1.0+0.0*mu);
-    case('flat_expMu')
+    case('flat-expMu')
       psi_MMS =@(x,mu) (1.0+0.0*x).*exp(mu);
       psi_MMS_Diff =@(x,mu) (0.0+0.0*x).*exp(mu);
-    case('flat_complex')
+    case('flat-complex')
       psi_MMS =@(x,mu) (1.0+0.0*x).*exp(cos(mu.*mu));
       psi_MMS_Diff =@(x,mu) (0.0+0.0*x).*exp(cos(mu.*mu));
-    case('sine')
+    case('sine-exp')
       psi_MMS =@(x,mu) sin(pi*x/Tau).*exp(mu);
       psi_MMS_Diff =@(x,mu) pi/Tau*cos(pi*x/Tau).*exp(mu);
   end
